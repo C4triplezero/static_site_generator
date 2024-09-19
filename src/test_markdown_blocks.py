63db0c99801f4d714aa2bf29ba9cdd07e23084f6
C4triplezero <jcurrie3000@gmail.com>
2024-09-18 22:49:54 -0600

    added file and function to convert markdown into blocks and testing for it

---
+++ src/test_markdown_blocks.py
@@ -0,0 +1,26 @@
+import unittest
+
+from markdown_blocks import markdown_to_blocks
+
+class TestMarkdownToHTML(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        markdown = """
+# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+"""
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(blocks, [
+            "# This is a heading", 
+            "This is a paragraph of text. It has some **bold** and *italic* words inside of it.", 
+            "* This is the first list item in a list block\n* This is a list item\n* This is another list item"
+        ])
+
+
+
+if __name__ == "__main__":
+    unittest.main()
