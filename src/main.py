6693b8b317ca5d543a2046712e6b7cb83768fc2c
C4triplezero <jcurrie3000@gmail.com>
2025-10-07 13:53:37 -0600

    fixed local hosting

--- src/main.py
+++ src/main.py
@@ -5,7 +5,9 @@ import sys
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
-basepath = sys.argv[1] if sys.argv[1] else "/"
+
+basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+
 dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
